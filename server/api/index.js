025c937d59efbb968965bd8c160a909139160c5f
Sam Narisi <snarisi@gmail.com>
2016-05-28 15:52:31 -0400

    added back end cache

--- server/api/index.js
+++ server/api/index.js
@@ -4,13 +4,24 @@ const router = require('express').Router();
 const fetch = require('node-fetch');
 const key = require('../../config/env').MOVIE_DB_KEY;
 
+const cache = {};
+
 router.get('/movies/:page', function (req, res, next) {
-	const query = `?api_key=${key}&page=${req.params.page}`;
+	const page = req.params.page;
+
+	if (cache[page]) res.json(cache[page]);
+	else next();
+});
+
+router.get('/movies/:page', function (req, res, next) {
+	const page = req.params.page;
+	const query = `?api_key=${key}&page=${page}`;
 	const url = `http://api.themoviedb.org/3/movie/now_playing${query}`;
 
 	fetch(url)
 		.then(response => response.json())
-		.then(response => res.json(response))
+		.then(response => cache[page] = response)
+		.then(() => res.json(cache[page]))
 		.catch(next);
 })
 
