8bdbdb9327b1425e9dc1a468e2b54d32e7485a05
Sam Narisi <snarisi@gmail.com>
2016-05-30 15:33:13 -0400

    finished detail modal

---
+++ client/movies/detail-modal/detail-modal.directive.js
@@ -0,0 +1,27 @@
+angular.module('movieGrid').directive('detailModal', function () {
+	return {
+		restrict: 'E',
+		scope: {
+			modalInfo: '=',
+			closeModal: '&onClose',
+			nextModal: '&onNext'
+		},
+		templateUrl: 'movies/detail-modal/detail-modal.html',
+		link: function (scope, el, attrs) {
+
+			console.log(scope);
+
+			scope.posterUrl = function (path) {
+				if (!path) return 'images/poster_placeholder.jpg';
+
+				return `http://image.tmdb.org/t/p/w185${path}`;
+			}
+
+			scope.getYear = function () {
+				if (!scope.modalInfo.movie) return '';
+
+				return scope.modalInfo.movie.release_date.split('-')[0];
+			}
+		}
+	}
+});
--- client/movies/movies.controller.js
+++ client/movies/movies.controller.js
@@ -2,6 +2,7 @@
 
 angular.module('movieGrid').controller('MoviesCtrl', function ($scope, movies) {
 	$scope.movies = movies.results;
+	console.log(movies);
 	$scope.page = {
 		current: movies.page,
 		total: movies.total_pages
@@ -12,4 +13,25 @@ angular.module('movieGrid').controller('MoviesCtrl', function ($scope, movies) {
 
 		return `http://image.tmdb.org/t/p/w185${path}`;
 	}
+
+	$scope.modalInfo = {
+		show: false,
+		index: null,
+		movie: null
+	};
+
+	$scope.openModal = function (idx) {
+		$scope.modalInfo.movie = $scope.movies[idx];
+		$scope.modalInfo.show = true;
+	};
+
+	$scope.closeModal = function () {
+		$scope.modalInfo.show = false;
+		$scope.modalInfo.movie = null;
+	};
+
+	$scope.nextModal = function () {
+		$scope.modalInfo.index++;
+		$scope.modalInfo.movie = $scope.movies[$scope.modalInfo.index];
+	};
 });
