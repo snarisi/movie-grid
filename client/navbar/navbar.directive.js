fb866eefd48749d36934d5ae0c0ad5bf32df31d3
Sam Narisi <snarisi@gmail.com>
2016-05-30 12:21:28 -0400

    styling movies page

---
+++ client/navbar/navbar.directive.js
@@ -0,0 +1,10 @@
+angular.module('movieGrid').directive('navbar', function () {
+	return {
+		restrict: 'E',
+		scope: {},
+		templateUrl: 'navbar/navbar.html',
+		link: function (scope, el, attrs) {
+			
+		}
+	}
+});
